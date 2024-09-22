2b6204f66642c6c263d3f4606206c2270e3e7c0f
Abdut777 <fahmidarahaman81@gmail.com>
2024-09-22 22:04:32 +0600

    bit

---
+++ seive prime.py
@@ -0,0 +1,6 @@
+def primeSeive(n):
+    prime=[true for i in range(n+1)]
+    currentNumber=2
+    while(currentNumber * currentNumber<=n):
+        if(prime[currentNmuber**2,n+1,currentNumber])
+        for i 
